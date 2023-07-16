1f4503d6e08d65401614ee15cd965b35016ad2cb
Eric Nygzarovich <09erik07@gmail.ru>
2023-07-16 20:02:29 +0300

    save changes

--- lib/modules/home/screens/home_screen.dart
+++ lib/modules/home/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wildtodo/core/core_utils.dart';
 
@@ -70,6 +73,8 @@ class _HomeNavigationScreenState extends State<HomeNavigationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.theme.palette.grayscale.g1,
+      drawer: const Drawer(),
+      appBar: getAppBar(context),
       body: Column(
         children: [
           Expanded(
@@ -110,5 +115,96 @@ class _HomeNavigationScreenState extends State<HomeNavigationScreen> {
         ],
       ),
     );
+  }
+}
+
+PreferredSizeWidget? getAppBar(BuildContext context) {
+  return AppBar(
+    iconTheme: IconThemeData(color: context.theme.palette.grayscale.g5),
+    backgroundColor: context.theme.palette.grayscale.g1,
+    title: Text(
+      'March 2023',
+      style: context.theme.typeface.subheading.bold,
+    ),
+    actions: [
+      Icon(
+        CupertinoIcons.bell_fill,
+        color: context.theme.palette.grayscale.g5,
+        size: 16,
+      ),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(16, 7, 24, 7),
+        child: SizedBox(
+          width: 42,
+          child: CustomPaint(
+            painter: ProgreesCircule(context: context, percent: 0.45),
+            child: Center(
+              child: ClipRRect(
+                clipBehavior: Clip.hardEdge,
+                borderRadius: const BorderRadius.all(Radius.circular(100)),
+                child: Image.asset(
+                  'assets/images/default_avata_image.png',
+                  fit: BoxFit.fill,
+                  height: 30,
+                  width: 30,
+                ),
+              ),
+            ),
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+class ProgreesCircule extends CustomPainter {
+  final BuildContext context;
+  final double percent;
+
+  ProgreesCircule({
+    required this.context,
+    required this.percent,
+  });
+
+  void _fillArcPaint(Canvas canvas, Size size) {
+    final arcFill = Paint();
+    arcFill.color = context.theme.palette.accent.secondary.vivid;
+    arcFill.style = PaintingStyle.stroke;
+    arcFill.strokeCap = StrokeCap.round;
+    arcFill.strokeWidth = 4;
+
+    canvas.drawArc(
+      const Offset(0, 0) & Size(size.width, size.height),
+      -pi / 2,
+      pi * 2 * percent,
+      false,
+      arcFill,
+    );
+  }
+
+  void _spaceArcPaint(Canvas canvas, Size size) {
+    final arcSpace = Paint();
+    arcSpace.color = context.theme.palette.grayscale.g5;
+    arcSpace.style = PaintingStyle.stroke;
+    arcSpace.strokeWidth = 4;
+
+    canvas.drawArc(
+      const Offset(0, 0) & Size(size.width, size.height),
+      pi * 2 * percent - (pi / 2),
+      pi * 2 * (1 - percent),
+      false,
+      arcSpace,
+    );
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    _spaceArcPaint(canvas, size);
+    _fillArcPaint(canvas, size);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
   }
 }
